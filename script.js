69fd1f528d93e033c5a5460722a7ec022645b963
yeryeong56 <2410522@hcu.hs.kr>
2024-07-13 14:05:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const questions = [
         explanation: "해설: 헌혈의 종류에는 전혈헌혈과 성분헌혈로 2가지 종류가 있습니다. 전혈헌혈이란, 혈액의 모든성분(적혈구, 백혈구, 혈장, 혈소판)을 헌혈하는 것이고, 성분헌혈이란, 혈액의 특정성분만을 헌혈하는 것입니다."
     },
     {
-        question: "3. 다음 글을 읽고 물음에 답하시오.\n ‘만 16세인 민권이는 헌혈을 하기 위해 헌혈의 집을 방문했다. 헌혈의 집에서 간호사는 민권이에게 만 16세는 (a) (b)헌혈이 가능하다고 했다.'\n 이때, (a)와 (b)에 들어갈 말로 알맞은 것을 고르시오.",
+        question: "3. 다음 글을 읽고 물음에 답하시오.\n ‘만 16세인 민수는 헌혈을 하기 위해 헌혈의 집을 방문했다. 헌혈의 집에서 간호사는 민수에게 만 16세는 (a) (b)헌혈이 가능하다고 했다.'\n 이때, (a)와 (b)에 들어갈 말로 알맞은 것을 고르시오.",
         choices: ["a. (a):혈소판, (b):성분", "b. (a):혈장, (b):성분", "c. (a):320mL, (b):전혈", "d. (a):360mL, (b):성분", "e. (a):400mL, (b):전혈"],
         answer: 'c',
         explanation: "해설: 만 16세는 320mL 전혈 헌혈만 가능합니다. 만 17세 이상부터는 모든 전혈헌혈 및 성분헌혈이 가능합니다."
@@ -40,6 +40,24 @@ const questions = [
         choices: ["a. 1명", "b. 2명", "c. 3명"],
         answer: 'c',
         explanation: "해설: 1회 400cc(400mL)의 헌혈로 3명의 생명을 살릴 수 있습니다!"
+    },
+    {
+        question: "8. 헌혈 후 받을 수 있는 헌혈 기념품으로 옳지 않은 것은?",
+        choices: ["a. 문화상품권", "b. 영화관람권", "c. 편의점교환권", "d. 커피교환권", "e. 대한적십자사 인형"],
+        answer: 'e',
+        explanation: "해설: 헌혈 후 받을 수 있는 헌혈 기념품의 종류에는 문화상품권, 영화관람권, 편의점교환권, 커피교환권 그리고 멀티모바일 문화상품권 등이 있습니다."
+    },
+    {
+        question: "9. 헌혈을 하면 빈혈에 걸린다? (o/x)",
+        choices: ["o", "x"],
+        answer: 'x',
+        explanation: "해설: 개인의 몸 상태에 따라서 일시적인 빈혈증세가 나타날 수는 있으나, 헌혈로 인해 무조건 빈혈에 걸린다는 말은 틀렸습니다."
+    },
+    {
+        question: "10. 헌혈이 심장마비를 감소시킨다? (o/x)",
+        choices: ["o", "x"],
+        answer: 'o',
+        explanation: "해설: 핀란드 쿠오피오에 있는 공중보건연구소는 영국 의학전문지〈브리티시 메디컬저널〉최신호에 \"2682명의 중년남자를 대상으로 조사한 결과, 헌혈 경험이 있는 사람이 그렇지 않은 사람에 비해 심장마비에 걸릴 확률이 80%나 낮다.\"라고 발표했습니다."
     }
 ];
 
@@ -55,7 +73,7 @@ const startScreen = document.getElementById('start-screen');
 const quizScreen = document.getElementById('quiz-screen');
 const resultScreen = document.getElementById('result-screen');
 const scoreElement = document.getElementById('score');
-const additionalInfoElement = document.createElement('p');
+const additionalInfoElement = document.getElementById('additional-info');
 
 startBtn.addEventListener('click', startQuiz);
 nextBtn.addEventListener('click', () => {
@@ -113,6 +131,8 @@ function selectAnswer(selectedChoice, correctAnswer, explanation) {
     }
     nextBtn.style.display = 'block';
 }
+
+
 
 function showResults() {
     quizScreen.style.display = 'none';
